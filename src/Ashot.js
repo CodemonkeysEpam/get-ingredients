7e927d5d7971e457f899a5b1a29c0456ada5756f
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-04-27 14:24:39 +0300

    Added carousel in ahots page, rendered 2 categories

--- src/Ashot.js
+++ src/Ashot.js
@@ -1,19 +1,64 @@
 import React from 'react';
+import './Ashot.scss';
+import ImageGallery from 'react-image-gallery';
 
 export default class Ashot extends React.Component {
-  render () {
 
-    let homeStyle = {
-      color: 'white',
-      fontSize: '35px',
-      textAlign: 'center',
-      marginTop: '20px'
-    }
+  renderShrink(){
+    return(
+      <div>
+        <div className="shrimp">
+          <div className="shrimp-body"></div>
+          <div className="tail-meat hovered"></div>
+        </div>
+        <div className="caption">Shrimp</div>
+      </div>
+    );
+  }
+
+  renderKnives(){
+    return (
+      <div>
+        <div className="knives">
+          <div className="knives-french-or-chef hovered"></div>
+          <div className="knives-boning hovered"></div>
+          <div className="knives-filleting hovered"></div>
+          <div className="knives-cleaver hovered"></div>
+          <div className="knives-carving hovered"></div>
+          <div className="knives-fork hovered"></div>
+          <div className="knives-slicing hovered"></div>
+          <div className="knives-scimitar hovered"></div>
+          <div className="knives-butcher hovered"></div>
+        </div>
+        <div className="caption">Knives</div>
+      </div>
+      
+    );
+  }
+
+  render () {
+    const images = [
+      {
+        renderItem: this.renderShrink,
+        thumbnail: 'icons/1x/shrink.png',
+        thumbnailClass: "meat-icons-item"
+      },
+      {
+        renderItem: this.renderKnives,
+        thumbnail: 'icons/1x/knives.png',
+        thumbnailClass: "meat-icons-item"
+      },
+    ]
 
     return (
-        <div>
-          <h1 style={homeStyle}>Ashot`s</h1>
-        </div>
+      <div className="carousel">
+      <ImageGallery 
+        items={images} 
+        thumbnailPosition="top" 
+        showFullscreenButton={false}
+        showPlayButton={false}
+        />
+      </div>
     );
   }
 }
