ce011f7d1dbb9d522f68c9f20e800f2731ded99c
Ivan Khud <anakin16@gmail.com>
2018-04-30 01:56:30 +0300

    added meals items and photos and places items

--- src/Menus.js
+++ src/Menus.js
@@ -1,8 +1,26 @@
 const MenusList = [
 
     {mealId : 1, placeId : 1, price : 45.6},
-    {mealId : 3, placeId : 1, price : 16.6},
-    {mealId : 1, placeId : 3, price : 44.6},
+    {mealId : 2, placeId : 18, price : 16.6},
+    {mealId : 3, placeId : 13, price : 44.6},
+    {mealId : 4, placeId : 5, price : 44.6},
+    {mealId : 5, placeId : 3, price : 44.6},
+    {mealId : 6, placeId : 7, price : 44.6},
+    {mealId : 7, placeId : 5, price : 44.6},
+    {mealId : 8, placeId : 5, price : 44.6},
+    {mealId : 9, placeId : 3, price : 44.6},
+    {mealId : 10, placeId : 3, price : 44.6},
+    {mealId : 11, placeId : 3, price : 44.6},
+    {mealId : 12, placeId : 3, price : 44.6},
+    {mealId : 13, placeId : 3, price : 44.6},
+    {mealId : 14, placeId : 3, price : 44.6},
+    {mealId : 15, placeId : 3, price : 44.6},
+    {mealId : 16, placeId : 3, price : 44.6},
+    {mealId : 17, placeId : 3, price : 44.6},
+    {mealId : 18, placeId : 3, price : 44.6},
+    {mealId : 19, placeId : 3, price : 44.6},
+    {mealId : 20, placeId : 3, price : 44.6},
+
 ]
 
 export default {
--- src/Places.js
+++ src/Places.js
@@ -1,12 +1,25 @@
 const PlacesList = [
 
-    {id: 1, name: 'Honest meat', address: 'Rynok sq 28', description: 'some description', lat: 49.845210, lng: 24.030431},
+    {id: 1, name: 'Honest Meat', address: 'Rynok sq 28', description: 'some description', lat: 49.845210, lng: 24.030431},
     {id: 2, name: 'Tex-Mex BBQ', address: 'Dudaeva 7', description: 'some description', lat: 49.836465, lng: 24.030287},
-    {id: 3, name: 'Kryva Lypa', address: 'Kryva Lypa', description: 'some description', lat: 49.840712, lng: 24.026686},
+    {id: 3, name: 'Kryva Lypa', address: 'Kryva Lypa 8', description: 'some description', lat: 49.840712, lng: 24.026686},
     {id: 4, name: 'Mons Pius', address: 'Lesi Ukrainky 14', description: 'some description', lat: 49.843607, lng: 24.031427},
-    {id: 5, name: 'Honest meat', address: 'Rynok sq 28', description: 'some description', lat: 49.845210, lng: 24.030431},
-    {id: 6, name: 'Honest meat', address: 'Rynok sq 28', description: 'some description', lat: 49.845210, lng: 24.030431}
-    
+    {id: 5, name: 'Meat and Justice', address: 'Valova 20', description: 'some description', lat: 49.840452, lng: 24.036741},
+    {id: 6, name: 'Arsenal. Ribs and Spirits', address: 'Pidvalna 5', description: 'some description', lat: 49.841434, lng: 24.035660},
+    {id: 7, name: 'The Most Expensive Restaurant', address: 'Rynok sq 14', description: 'some description', lat: 49.841367, lng: 24.032290},
+    {id: 8, name: 'Arsenal. Ribs and Spirits', address: 'Pidvalna 5', description: 'some description', lat: 49.841434, lng: 24.035660},
+    {id: 9, name: 'Churasco', address: 'Galytska 13', description: 'some description', lat: 49.841367, lng: 24.032290},
+    {id: 10, name: 'Meat and Burger', address: 'Gnatyuka 12', description: 'some description', lat: 49.841767, lng: 24.026025},
+    {id: 11, name: 'The Grill', address: 'Teatralna 22', description: 'some description', lat: 49.843410, lng: 24.029206},
+    {id: 12, name: 'Kumpel', address: 'Vynnychenka 6', description: 'some description', lat: 49.839430, lng: 24.036481},
+    {id: 13, name: 'Museum of Ideas', address: 'Valova 18a', description: 'some description', lat: 49.840030, lng: 24.035388},
+    {id: 14, name: 'Gas Lamp', address: 'Virmenska 20', description: 'some description', lat: 49.843259, lng: 24.032540},
+    {id: 15, name: 'Kentaur', address: 'Rynok sq 34', description: 'some description', lat: 49.842558, lng: 24.031148},
+    {id: 16, name: 'Bachevsky Restaurant', address: 'Shevska, 8', description: 'some description', lat: 49.842365, lng: 24.030786},
+    {id: 17, name: 'Koghut Hosper Pub', address: 'Chornovola 2', description: 'some description', lat: 49.847619, lng: 24.026883},
+    {id: 18, name: 'Amadeus', address: 'Kathedralna 7', description: 'some description', lat: 49.840569, lng: 24.031185},
+    {id: 19, name: 'Scotland Cafe', address: 'Shevchenka 27', description: 'some description', lat: 49.835939, lng: 24.033171},
+
 ]
 
 export default {
