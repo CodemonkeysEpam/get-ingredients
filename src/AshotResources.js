29d8252c8de1292a9ef7f26f32f1ec02bcb2bb95
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-05-01 12:18:27 +0300

    Created AshotResources file, added functionality to click on animals part and show recepies

---
+++ src/AshotResources.js
@@ -0,0 +1,120 @@
+const Recepies = [
+    // BEAR
+    {
+      "head": `HEAD`,
+      "ear": "EAR",
+      "neck": "NECK",
+      "chuck": "CHUCK",
+      "rib": "RIB",
+      "shotr-loin": "shotr-loin",
+      "loin": "loin",
+      "rump": "RUMP",
+      "tail": "TAIL",
+      "brisket": "brisket",
+      "shoulder": "shoulder",
+      "short-ribs":"short-ribs",
+      "plate":"plate",
+      "flank":"flank",
+      "round":"round",
+      "fore-shank":"fore-shank",
+      "fore-shank-1":"fore-shank-1",
+      "hind-shank":"hind-shank",
+      "hind-shank-1":"hind-shank-1",
+      img: '/ashot-images/bear.jpg'
+    },
+    // BEEF
+    {
+      "head": `HEAD`,
+      "chuck": "CHUCK",
+      "rib":"RIB",
+      "loin":"LOIN",
+      "sirloin":"sirloin",
+      "round":"ROUND",
+      "brisket": "BRISKET",
+      "plate":"PLATE",
+      "flank":"FLANK",
+      "shank":"SHANK",
+      img: '/ashot-images/beef.png'
+    },
+    // PORK 
+    {
+      "head": `HEAD`,
+      "arm-shoulder": "arm-shoulder",
+      "blade-shoulder": "blade-shoulder",
+      "hock": "hock",
+      "loin":"LOIN",
+      "spare-rib":"spare-rib",
+      "side":"side",
+      "leg":"leg",
+      img: '/ashot-images/pork.jpg'
+    },
+    // CHICKEN
+    {
+      "neck": `neck`,
+      "breast": "BREAST",
+      "leg":"LEG",
+      "wing":"WING",
+      "tail":"TAIL",
+      "thign":"THIGN",
+      img: '/ashot-images/pork.jpg'
+    },
+    // LAMB
+    {
+      "neck": `NECK`,
+      "chuck":"CHUCK",
+      "sholder":"sholder",
+      "breast":"breast",
+      "loin":"loin",
+      "leg":"leg",
+      img: '/ashot-images/pork.jpg'
+    },
+    // RABBIT
+    {
+      "shoulder": `shoulder`,
+      "front-leg":"front-leg",
+      "saddle":"saddle",
+      "rib":"rib",
+      "loin":"loin",
+      "hind-leg":"LEG",
+      img: '/ashot-images/pork.jpg'
+    },
+    // FISH
+    {
+      "head": `HEAD`,
+      "back":"BACK",
+      "caviar":"CAVIAR",
+      "abdomen-meat":"abdomen-meat",
+      "tail-meat":"MEAT",
+      img: '/ashot-images/pork.jpg'
+    },
+    // CRAB
+    {
+      "claw-meat": `CLAW-MEAT`,
+      "white-meat": "WHITE-MEAT",
+      img: '/ashot-images/pork.jpg'
+    },
+    // SHRINK
+    {
+      "tail-meat": `tail-meat`,
+      img: '/ashot-images/pork.jpg'
+    },
+    // KNIVES
+    {
+      "knives-french-or-chef": `knives-french-or-chef`,
+      "knives-boning":"knives-boning",
+      "knives-filleting":"knives-filleting",
+      "knives-cleaver":"knives-cleaver",
+      "knives-carving":"knives-carving",
+      "knives-fork":"knives-fork",
+      "knives-slicing":"knives-slicing",
+      "knives-scimitar":"knives-scimitar",
+      "knives-butcher":"knives-butcher",
+      img: '/ashot-images/pork.jpg'
+    },
+]
+
+
+export default {
+    Recepies
+}
+
