ebd08d908229d8e844d9241b67691b023234db60
Jurek Krut <jurekkrut@gmail.com>
2018-05-06 14:29:36 +0300

    added map and styles to contact-us component

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './styles/ContactUs.scss';
+import GoogleMapReact from 'google-map-react'
+
+const Marker = (props) => <div style={{"background": "url(https://image.flaticon.com/icons/svg/33/33622.svg)", "width": "25px", "height": "25px", "backgroundSize": "100%"}}></div>;
 
 export default class ContactUs extends React.Component {
     render() {
-        return(
+        return (
             <div className="contact-us">
-                <h3>
-                    We are crazy about meat. Beef. Pork. Veal. Chicken. Venison. Doesn't matter. This is our lifestyle and this is how we live.
-We are challenging modern vegetarian culture. We are underground. Like us? Join us.
-                </h3>
-                <div id="map">
+                <h3>We are crazy about meat. Beef. Pork. Veal. Chicken. Venison. Doesnt matter. This is our lifestyle and this is how we live. We are challenging modern vegetarian culture. We are underground. Like us? Join us.</h3>
+                <div className="map" style={{ height: '400px', width: '60%' }}>
+                    <GoogleMapReact
+                      bootstrapURLKeys={{ key: 'AIzaSyBR6d-Nuc7yQLspw41q5WREpj--vWNQdJo' }}
+                      defaultCenter={{lat: 49.8426, lng: 23.9997}}
+                      defaultZoom={15}>
+                      <Marker
+                        lat={49.842660}
+                        lng={23.999795}
+                      />
+                    </GoogleMapReact>
                 </div>
                 <div className="contacts">
                     <address>
