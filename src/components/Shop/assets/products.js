2db882ecc5267c3ca50e3471b4afef77f1987ed4
Olena Rukholla <ruholkin@gmail.com>
2018-05-10 12:21:38 +0300

    add Shop resources

---
+++ src/components/Shop/assets/products.js
@@ -0,0 +1,16 @@
+import Sizes from './sizes.js';
+
+const Products = [
+
+	{ id: 0, name: 'T-shirt', src: '', sizes: Sizes, price: 5, },
+	{ id: 1, name: 'Bag', src: '', price: 1},
+	{ id: 2, name: 'Badge', src: '', price: 1},
+	{ id: 3, name: 'Notebook', src: '', price: 3},
+	{ id: 4, name: 'Pen', src: '', price: 2},
+	{ id: 5, name: 'Flash-card', src: '', price: 4}
+
+];
+
+export default {
+	Products
+}
---
+++ src/components/Shop/assets/sizes.js
@@ -0,0 +1,12 @@
+const Sizes = [
+
+	{ id: 0, size: 'XS'},
+	{ id: 0, size: 'S'},
+	{ id: 0, size: 'M'},
+	{ id: 0, size: 'L'},
+	{ id: 0, size: 'XL'},
+	{ id: 0, size: 'XXL'}
+
+];
+
+export default Sizes;
