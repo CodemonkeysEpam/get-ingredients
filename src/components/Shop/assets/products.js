ecaf3b757330383f53f9a7eedfdb53eee3971826
Olena Rukholla <ruholkin@gmail.com>
2018-05-10 16:25:54 +0300

    add src images

--- src/components/Shop/assets/products.js
+++ src/components/Shop/assets/products.js
@@ -2,12 +2,12 @@ import Sizes from './sizes.js';
 
 const Products = [
 
-	{ id: 0, name: 'T-shirt', src: '', sizes: Sizes, price: 5, },
-	{ id: 1, name: 'Bag', src: '', price: 1},
-	{ id: 2, name: 'Badge', src: '', price: 1},
-	{ id: 3, name: 'Notebook', src: '', price: 3},
-	{ id: 4, name: 'Pen', src: '', price: 2},
-	{ id: 5, name: 'Flash-card', src: '', price: 4}
+	{ id: 0, name: 'T-shirt', src: '/img/shop/products/T-shirt-0.png', sizes: Sizes, price: 5, },
+	{ id: 1, name: 'Bag', src: '/img/shop/products/Bag-0.png', price: 1},
+	{ id: 2, name: 'Badge', src: '/img/shop/products/Badge-0.png', price: 1},
+	{ id: 3, name: 'Notebook', src: '/img/shop/products/Notebook-0.png', price: 3},
+	{ id: 4, name: 'Pen', src: '/img/shop/products/Pen-0.png', price: 2},
+	{ id: 5, name: 'Flash-card', src: '/img/shop/products/Flash-card-0.png', price: 4}
 
 ];
 
