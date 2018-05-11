276efbe1d8c6e1700dde429b44080d46c59c3af1
Jurek Krut <jurekkrut@gmail.com>
2018-05-11 11:33:31 +0300

    added container wraper to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,14 @@ export default class Footer extends React.Component {
     render() {
         return(
             <footer>
+                <div className="container-wrapper">
                 <ul className="left-column">
                     <li><Link to="/contact-us">Contact Us</Link></li>
                     <li>
                         <div className="social-networks">
-                            <a href="https://github.com/CodemonkeysEpam" target="_blank"><div className="github"></div></a>
-                            <a href="https://fb.com" target="_blank"><div className="facebook"></div></a>
-                            <a href="https://instagram.com" target="_blank"><div className="instagram"></div></a>
+                            <Link to="https://github.com/CodemonkeysEpam" target="_blank"><div className="github"></div></Link>
+                            <Link to="https://fb.com" target="_blank"><div className="facebook"></div></Link>
+                            <Link to="https://instagram.com" target="_blank"><div className="instagram"></div></Link>
                         </div>
                     </li>
                     <li><Link to="/recepies">Our partners</Link></li>
@@ -23,6 +24,7 @@ export default class Footer extends React.Component {
                     <li><Link className="title" to="/">Meat is life</Link></li>
                     <li>Codemonkeys team 2018</li>
                 </ul>
+                </div>
             </footer>
         )
     }
