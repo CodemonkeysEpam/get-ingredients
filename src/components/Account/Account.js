215f527bf3750e977e12d334b953f29ec6c53a5c
Ivan Boyko <boykoivan1998@gmail.com>
2018-05-11 20:30:09 +0300

    add ability to change profile name

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -12,7 +12,8 @@ class Account extends React.Component {
 
       this.state = {
           currentTab: 'MyInfo',
-          user: firebase.auth().currentUser
+          user: firebase.auth().currentUser,
+          isEditName: false
       }
   }
 
@@ -76,6 +77,26 @@ changeImage = (event) => {
     })
 }
 
+changeDispayName = () => {
+    this.state.user.updateProfile({
+        displayName: this.displayNameInput.value
+      }).then(function() {
+        //   this.forceUpdate();
+          this.setState({
+            isEditName: false
+        })
+
+      }.bind(this)).catch(function(error) {
+          console.log(error);
+      });
+}
+
+showEditName = () => {
+    this.setState({
+        isEditName: true
+    })
+}
+
 render () {
     return (
         <div className="main-section">
@@ -83,12 +104,24 @@ render () {
             <div className="container">
                 <div className="account-body">
                     <div className="left-sidebar">
-                    <div className="logo">
-                    <img src={this.getPhotoURL(this.state.user.photoURL)} alt="Profile" />
-                    <label htmlFor="file-input">
-                        <div className="change-button"><span>Change photo</span></div>
-                    </label>
-                    <input id="file-input" type="file" onChange={this.changeImage} accept="image/*"/>
+                    <div className="account-info">
+                        <div className="logo">
+                            <img src={this.getPhotoURL(this.state.user.photoURL)} alt="Profile" />
+                            <label htmlFor="file-input">
+                                <div className="change-button"><span>Change photo</span></div>
+                            </label>
+                            <input id="file-input" type="file" onChange={this.changeImage} accept="image/*"/>
+                        </div>
+                        <div className="displayName">
+                            {this.state.isEditName ?
+                                <input type="text" autoFocus ref={input => this.displayNameInput = input} defaultValue={this.state.user.displayName} onBlur={this.changeDispayName}/>
+                                :
+                                <React.Fragment>
+                                    <span className="text">{this.state.user.displayName}</span>
+                                    <i className="fa fa-pencil" onClick={this.showEditName}></i>
+                                </React.Fragment>
+                            }
+                        </div>
                     </div>
                     <a href="#" onClick={() => this.handleClick('MyInfo')}>My Info</a> | 
                     <a href="#" onClick={() => this.handleClick('MyOrders')}>My orders</a> |
