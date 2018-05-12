133f069ffa506fe4b9895af5b87e17853cf5e710
Ivan Khud <anakin16@gmail.com>
2018-05-12 19:55:31 +0300

    css improvements of Restaurant and FindLocation components

--- src/components/RestaurantItem/RestaurantItem.js
+++ src/components/RestaurantItem/RestaurantItem.js
@@ -6,8 +6,8 @@ const RestaurantItem = (props) => {
         return (
             <div className="restaurant">
                 <img className="restaurant-logo" src={props.logoUrl} alt={props.name} />
-                <div className="place-name">{props.name}</div>
-                {props.address && <div className="place-address">{props.address}</div>}
+                <div className="restaurant-name">{props.name}</div>
+                {props.address && <div className="restaurant-address">{props.address}</div>}
                 {props.showOnMapClick && <button className="restaurant-item-button" onClick={props.showOnMapClick}>Show on map</button>}
                 <Link to={`/restaurant/${props.id}`}><button className="restaurant-item-button" onClick={props.detailsClick}>Details</button></Link>
             </div>
