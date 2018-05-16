bad6cdb3e7cd10fcf0627206b9db716d93f92991
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-05-16 22:17:51 +0300

    Updated Dashboard in Account page

--- src/components/Account/Dashboard/Dashboard.js
+++ src/components/Account/Dashboard/Dashboard.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './Dashboard.scss';
+
 
 export default class MyInfo extends React.Component {
     render() {
         return (
-            <div>
-               All orders: 5, Waiting shipment: 0, Received: 5
+            <div className="dashboard">
+               {/* All orders: 5, Waiting shipment: 0, Received: 5
                <br/>
-               Recomended:
+               Recomended: */}
+               <div className="dashboard-item">
+                <div>All orders:</div>
+                <div>5</div>
+               </div>
+               <div className="dashboard-item">
+                <div>Waiting shipment:</div>
+                <div>0</div>
+               </div>
+               <div className="dashboard-item">
+                <div>Received:</div>
+                <div>5</div>
+               </div>
+
             </div>
         )
     }
