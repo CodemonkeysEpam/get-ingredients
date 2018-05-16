46544b9a8c7cd15690895245543cacad8ed486f1
Olena Rukholla <ruholkin@gmail.com>
2018-05-17 02:30:33 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/CodemonkeysEpam/meatIsLife""
    This reverts commit b1f34e3c0b4a99a5b42c9547f3bd360ec2fa6b3d.

--- src/components/Account/Partners/Partners.js
+++ src/components/Account/Partners/Partners.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import base from '../../../services/base';
 import AddPartners from "./AddPartners/AddPartners";
-import AddProduct from "./PartnersItem/Products/AddProduct";
 import PartnersItem from "./PartnersItem/PartnersItem";
 import { Link } from 'react-router-dom';
 import { withRouter } from "react-router";
@@ -13,9 +12,7 @@ class Partners extends React.Component {
         this.state = {
             currentTab: 'Partners',
             placesList: [],
-            shopsList: [],
-            mealsList: [],
-            menusList: []
+            shopsList: []
         }
     }
   
@@ -29,30 +26,26 @@ class Partners extends React.Component {
         this.refPlaces = base.bindToState(`meals/places`, {
             context: this,
             state: 'placesList',
-            asArray: true
+            asArray: true,
+            queries: {
+                orderByChild: 'userId',
+                equalTo: this.props.uid
+            }
           });
           this.refShops = base.bindToState(`meat/places`, {
             context: this,
             state: 'shopsList',
-            asArray: true
-          });
-          this.refMeals = base.bindToState(`meals/meals`, {
-            context: this,
-            state: 'mealsList',
-            asArray: true
-          });
-          this.refMenus = base.bindToState(`meals/menus`, {
-            context: this,
-            state: 'menusList',
-            asArray: true
+            asArray: true,
+            queries: {
+                orderByChild: 'userId',
+                equalTo: this.props.uid
+            }
           });
       }
     
       componentWillUnmount() {
           base.removeBinding(this.refPlaces);
           base.removeBinding(this.refShops);
-          base.removeBinding(this.refMeals);
-          base.removeBinding(this.refMenus);
       }
 
       static getDerivedStateFromProps(nextProps, prevState) {
@@ -65,8 +58,6 @@ class Partners extends React.Component {
         }
         else if(nextProps.location.pathname.includes("/account/partners/shops/")) {
             currentTab = "PartnersShopItem"
-        } else if(nextProps.location.pathname.includes("/account/partners/meals/")) {
-            currentTab = "PartnersMealItem"
         }
         else {
             currentTab = "Partners"
@@ -123,27 +114,6 @@ class Partners extends React.Component {
                         <div>No data</div>
                         }
                     </div>
-
-                    <div className="type-container">
-                        <div className="header">
-                            <div className="title">Products:</div>
-                            <Link to="/account/partners/meals/" className="add-button">Add new item</Link>
-                        </div>
-                        
-                        {this.state.shopsList.length > 0 ?
-                        this.state.mealsList.map((meal, index) => (
-                            <Link to={`/account/partners/meals/${meal.id}`} className="item" key={meal.id}>
-                                <img src="https://firebasestorage.googleapis.com/v0/b/meatislifeepam.appspot.com/o/default%2Fprofile.jpg?alt=media&token=d26705f2-7d77-4c1e-b628-9cc1bd1a69e2" alt="logo"/>
-                                <div className="body">
-                                    <div className="title">{meal.name}</div>
-                                    <div className="verified">Verified: {meal.verified.toString()}</div>
-                                </div>
-                            </Link>
-                        ))
-                        :
-                        <div>No data</div>
-                        }
-                    </div>
                 </React.Fragment>
             )
         } else if(this.state.currentTab === "AddPartners"){
@@ -159,10 +129,6 @@ class Partners extends React.Component {
         else if(this.state.currentTab === "PartnersShopItem") {
             return (        
                 <PartnersItem uid={this.props.uid} type="shop"/>
-            )
-        } else if(this.state.currentTab === "PartnersMealItem") {
-            return (
-                <AddProduct uid={this.props.uid} type="meal" mealsList={this.state.mealsList} menusList={this.state.menusList} />
             )
         }
 
--- src/components/Account/Partners/PartnersItem/Products/Products.js
+++ src/components/Account/Partners/PartnersItem/Products/Products.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import { withRouter } from "react-router";
+import { Link } from 'react-router-dom';
+import AddProduct from './AddProduct';
+import base from '../../../../../services/base';
 
-export default class Products extends React.Component {
+class Products extends React.Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+            currentTab: 'Products',
+            mealsList: [],
+            menusList: []
+        }
+    }
+
+    componentDidMount() {
+        this.refMeals = base.bindToState(`meals/meals`, {
+          context: this,
+          state: 'mealsList',
+          asArray: true
+        });
+        this.refMenus = base.bindToState(`meals/menus`, {
+          context: this,
+          state: 'menusList',
+          asArray: true
+        });
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.refMeals);
+        base.removeBinding(this.refMenus);
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        var currentTab;
+        if(nextProps.location.pathname.includes("/add")) {
+            currentTab = "AddProduct"
+        }
+        else {
+            currentTab = "Products"
+        }
+        return {
+            currentTab
+        };
+    }
+
     render() {
-        return <div>products</div>
+        return this.state.currentTab === "AddProduct" ? <AddProduct mealsList={this.state.mealsList} placeId={this.props.placeId}/> :
+        <div>
+            products
+            <Link to={`${this.props.location.pathname}/add`}>add</Link>
+        </div>
     }
 }
+
+export default withRouter(Products)
