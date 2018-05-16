84c2d598e51fc486e4c9495902a012178b22e4fd
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-05-16 23:57:58 +0300

    Added inputs to Info

--- src/components/Account/Partners/PartnersItem/Dashboard/Dashboard.js
+++ src/components/Account/Partners/PartnersItem/Dashboard/Dashboard.js
@@ -19,8 +19,41 @@ export default class Dashboard extends React.Component{
                     </div>
                 </div>
                 <div className="partners-dash-info">
-                    Info
-                </div>
+                    <div className="add-partner">
+                        <div className="label">
+                            <div className="title">Name:</div>
+                            <input type="text"  placeholder="Enter name"/>
+                        </div>
+                        <div className="label">
+                            <div className="title">Address:</div>
+                            <input type="text"  placeholder="Enter name"/>
+                        </div>
+                        <div className="label">
+                            <div className="title">Phone number:</div>
+                            <input type="text" placeholder="Enter phone number"  />
+                        </div>
+                        <div className="label">
+                            <div className="title">Photo:</div>
+                            <label htmlFor="file-add-partner">
+                                    <div className="file">
+                                        <span>
+                                        Choose a file...
+                                        {/* {this.state.file === null ? 
+                                        "Choose a file..." : 
+                                        this.state.file.name} */}
+
+                                        </span>
+                                    </div>
+                            </label>
+                            <input type="file" id="file-add-partner" accept="image/*" />
+                        </div>
+                        
+                        <div className="label">
+                            <div className="title">Description:</div>
+                            <textarea placeholder="Enter description" ></textarea>
+                        </div>
+                    </div>
+                </div>                            
             </div>
         );
     }
